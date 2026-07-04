14e17bbd38ee6e11ac5560d355be2447ae06051c
Murgu Andrei <murgua03@gmail.com>
2026-07-04 15:03:28 +0300

    removed pressure sensitivity immitation

--- src/input_manager.c
+++ src/input_manager.c
@@ -48,13 +48,7 @@ void ProcessInputs(Document* doc, Viewport* vp, bool guiClicked, int* draggedPag
         if(IsUsingTablet()){
             *currentPressure = GetTabletPressure();
         } else{
-            if (doc->activeBrush <= BRUSH_PENCIL && currentStroke.pointCount > 0) {
-                float dist = Vector2Distance(vp->localMousePos, currentStroke.points[currentStroke.pointCount - 1].pos);
-                float targetPressure = 1.0f - (dist / 30.0f);
-                if (targetPressure < 0.1f) targetPressure = 0.1f;
-                if (targetPressure > 1.0f) targetPressure = 1.0f;
-                *currentPressure = (*currentPressure * 0.7f) + (targetPressure * 0.3f);
-            }
+            *currentPressure = 1;
         }
         GetActiveBrush()->OnDrag(doc, clampedPos, *currentPressure);
 
